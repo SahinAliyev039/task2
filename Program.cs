7730bc538982961c430eb86511ef5cf6966c6235
SahinAliyev039 <shahinsa@code.edu.az>
2024-02-17 11:19:15 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Elvin";
+            string surname = "Vahidli";
+            Console.WriteLine(name + " " + surname);
+            float num1 = 4;
+            float num2 = 6;
+            float num3 = num1 + num2;
+            Console.WriteLine(num3);
+            float num4 = 2;
+            float num5 = 7;
+            float num6 = num4 - num5;
+            Console.WriteLine(num6);
+            float num7 = num1 * num2;
+            float num8 = num7 * num7; 
+            Console.WriteLine(num8);
+            float num9 = num5 / num4;
+            Console.WriteLine(num9);
+            float num10 = num9 * num9;
+            Console.WriteLine(num10);
+
+
+            Console.ReadKey();
+        }
+    }
+}
